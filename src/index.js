3a7807cd5a98cbd2d4a2b98c7c076dd432e84e5f
Marco 'Gatto' Boffo <marcoboffo.waves@gmail.com>
2019-06-10 14:43:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Scrollbar from 'smooth-scrollbar'
+import './default.css'
+import { bestListener, bestOptions } from './utils'
+
+export default {
+  install (Vue, options) {
+    // helpers
+    Vue.prototype.$scrollbar = Scrollbar
+    Vue.scrollbar = Scrollbar
+
+    // directive
+    Vue.directive('smoothscrollbar', {
+      bind (el) {
+        el.classList.add('smooth-vuebar')
+      },
+      inserted (el, binding) {
+        const possibilities = [binding.value, options]
+        const scrollbar = Scrollbar.init(el, bestOptions(possibilities))
+
+        const listener = bestListener(possibilities)
+        if (listener) scrollbar.addListener(listener)
+      },
+      componentUpdated (el, binding) {
+        const scrollbar = Scrollbar.get(el)
+        if (!scrollbar) return
+
+        // remove old listener, if defined
+        const oldListener = bestListener([binding.oldValue, options])
+        if (oldListener) scrollbar.removeListener(oldListener)
+
+        // add the new listener, if defined
+        const listener = bestListener([binding.value, options])
+        if (listener) scrollbar.addListener(listener)
+
+        // refresh
+        scrollbar.update()
+      },
+      unbind (el) {
+        const scrollbar = Scrollbar.get(el)
+        if (scrollbar) scrollbar.destroy()
+      }
+    })
+  }
+}
